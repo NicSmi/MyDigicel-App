b8400ca95f1e29079934ad6b91a9eaca0bb27409
NicSmi <38170669+NicSmi@users.noreply.github.com>
2020-03-07 14:12:54 -0500

    updating app

--- mydigiapp/lib/main.dart
+++ mydigiapp/lib/main.dart
@@ -7,7 +7,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'MyDigicel',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -18,25 +19,15 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'MyDigicel'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,47 +39,80 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Nicoy Smith'),
+              decoration: BoxDecoration(
+                color: Colors.redAccent,
+              ),
+              ),
+              ListTile(
+                title: Text('Settings',style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.bold ),),
+                onTap: (){
+
+                },
+              ),
+              ListTile(
+                title: Text('Tutorial'),
+                onTap: (){
+
+                },
+              ),
+              ListTile(
+                title: Text('Shake It'),
+                onTap: (){
+
+                },
+              ),
+              ListTile(
+                title: Text('Account & Balances'),
+                onTap: (){
+
+                },
+              ),
+              ListTile(
+                title: Text('Offers for You'),
+                onTap: (){
+
+                }
+              ),
+              ListTile(
+                title: Text('Shared Data'),
+                onTap: (){
+
+              },),
+              ListTile(
+                title: Text('Games & More'),
+                onTap: (){
+
+                }
+              ),
+              ListTile(
+                title: Text('Additional Services'),
+                onTap: (){
+
+                }
+              )
+
+          ],
+        ),
+      ),
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        //backgroundColor: Colors.white,
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
@@ -105,7 +129,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
